a8945bcd0c62ceab5e97cfcb231e030b2a5e387c
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-05-20 23:59:21 -0700

    Adds remaining necessary functions

--- DAOS/parkDao.js
+++ DAOS/parkDao.js
@@ -1,9 +1,13 @@
 // Import the model:
 const Park = require("../models/parkModel")
 
+
+// ** NOTE:  All of these are restricted to admin only use! **
+
+
 ////////////////////////
 // CREATE a new park //
-////////////////////////
+//////////////////////
 async function createPark(data) {
   const park = new Park(data);
   return await park.save();
@@ -16,8 +20,40 @@ async function getAllParks() {
   return await Park.find();
 }
 
+///////////////////////////////
+// READ/Find parks by state //
+/////////////////////////////
+async function getParksByState(state) {
+  return await Park.find({ state: state.toUpperCase() });
+}
+
+///////////////////////////////
+// READ/Find one park by ID //
+/////////////////////////////
+async function getParkById(id) {
+  return await Park.findById(id);
+}
+
+///////////////////////////
+// UPDATE a park's info //
+/////////////////////////
+async function updatePark(id, updates) {
+  return await Park.findByIdAndUpdate(id, updates, { new: true });
+}
+
+////////////////////
+// DELETE a park //
+//////////////////
+async function deletePark(id) {
+  return await Park.findByIdAndDelete(id);
+}
+
 
 module.exports = {
     createPark,
     getAllParks,
-}
+    getParksByState,
+    getParkById,
+    updatePark,
+    deletePark,
+};
