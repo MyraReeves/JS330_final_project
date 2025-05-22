791401b070a7301689dff8582ded2b1d827522e4
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-05-22 01:49:29 -0700

    Adds remaining routes

--- routes/visited.js
+++ routes/visited.js
@@ -6,6 +6,28 @@ const isAdmin = require("../middleware/isAdmin");
 const router = express.Router();
 
 
+/////////////////////////////////////////////////////////////////
+// CREATE a "visited parks" list if the user doesn't have one //
+///////////////////////////////////////////////////////////////
+router.post("/", isAuthorized, async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const initialParks = req.body.parks || [];
+        const existing = await visitedDao.getVisitedByUserId(userId);
+
+        // If a list already exists for that user, throw an error:
+        if (existing) return res.status(409).json({ message: "A list of visited parks already exists!" });
+
+        // Otherwise, create a list using the inputed intial parks that were in the request body:
+        const created = await visitedDao.createVisitedList(userId, initialParks);
+        res.status(201).json(created);
+    } 
+    catch (err) {
+        res.sendStatus(500);
+    }
+});
+
+
 //////////////////////////////////////////////////////
 // READ all visited parks for a specified username //
 ////////////////////////////////////////////////////
@@ -13,7 +35,10 @@ router.get("/username/:username", async (req, res) => {
     try {
         const visited = await visitedDao.getVisitedByUsername(req.params.username);
 
-        if (!visited) return res.status(404).json({ message: "User not found or no visited parks." });
+        // If nothing can be found return a 404 error:
+        if (!visited) return res.status(404).json({ message: "The requested user doesn't exist or has not yet created a list" });
+
+        // Otherwise, return their list:
         res.json(visited);
     }
     catch (err) {
@@ -29,7 +54,10 @@ router.get("/username/:username/type/:type", async (req, res) => {
     try {
         const parks = await visitedDao.getVisitedByType(req.params.username, req.params.type);
 
-        if (!parks) return res.status(404).json({ message: "User not found or no matching parks." });
+        // If nothing can be found return a 404 error:
+        if (!parks) return res.status(404).json({ message: "Could not find any record of that username having visited any parks of that type" });
+
+        // Otherwise, return a list of all the parks they've visited of that type:
         res.json(parks);
     }
     catch (err) {
@@ -45,10 +73,51 @@ router.get("/username/:username/state/:state", async (req, res) => {
     try {
         const parks = await visitedDao.getVisitedByState(req.params.username, req.params.state);
 
-        if (!parks) return res.status(404).json({ message: "User not found or no parks in that state." });
+        if (!parks) return res.status(404).json({ message: "Could not find any matches for that username and that state" });
+
         res.json(parks);
     }
     catch (err) {
         res.sendStatus(500);
     }
 });
+
+
+///////////////////////////////////////////////////////////////
+// UPDATE - Add a new park to a user's "visited parks" list //
+/////////////////////////////////////////////////////////////
+router.put("/add/:parkId", isAuthorized, async (req, res) => {
+    try {
+        const updated = await visitedDao.addParkToVisited(req.user._id, req.params.parkId);
+
+        // Check whether a list exists for that user. If not, return an error:
+        if (!updated) return res.status(404).json({ message: "List not found" });
+
+        // Otherwise, send the update:
+        res.json(updated);
+    } 
+    catch (err) {
+        res.sendStatus(500);
+    }
+});
+
+
+///////////////////////////////////////////////////////////////
+// DELETE a park from the user's list (in case of mistakes) //
+/////////////////////////////////////////////////////////////
+router.delete("/remove/:parkId", isAuthorized, async (req, res) => {
+    try {
+        const updated = await visitedDao.removeParkFromVisited(req.user._id, req.params.parkId);
+    
+        if (!updated) return res.sendStatus(404);
+
+        res.json(updated);
+    }
+    catch (err) {
+        res.sendStatus(500);
+    }
+});
+
+
+
+module.exports = router;
