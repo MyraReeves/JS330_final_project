548a3aeabe320364ea077fb97a2867d51756a056
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-05-22 03:30:02 -0700

    Adds additional remaining components of the schema as well as timestamps

--- models/commentModel.js
+++ models/commentModel.js
@@ -7,7 +7,57 @@ const commentsSchema = new mongoose.Schema({
     required: true,                           // UserId is required
   },
 
-});
+  parkId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Park",
+    required: true,
+    },
+
+  state: {
+    type: String,
+    enum: ["WA", "OR"],
+    required: true,
+  },
+
+  visitDate: {
+    type: Date,
+    required: true,
+  },
+
+  weather: {
+    type: String,
+    trim: true,
+  },
+
+  tripSummary: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+
+  additionalNotes: {
+    type: String,
+    trim: true,
+  },
+
+  favoriteMoment: {
+    type: String,
+    trim: true,
+  },
+
+  memorableSights: {
+    type: String,
+    trim: true,
+  },
+
+  helpfulTips: {
+    type: String,
+    trim: true,
+  }
+
+}, { timestamps: true }     // Logs "createdAt" and "updatedAt" timestamps
+);
+
 
 
 module.exports = mongoose.model("Comment", commentsSchema);
