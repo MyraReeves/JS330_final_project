6404db29146e80f8b0e5cae37f9c7b1b67be6619
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-05-21 22:56:44 -0700

    Adds parkName, state, and parkType

--- models/visitedModel.js
+++ models/visitedModel.js
@@ -2,17 +2,35 @@ const mongoose = require("mongoose");
 
 const visitedSchema = new mongoose.Schema({
   userId: {
-    type: mongoose.Schema.Types.ObjectId,     // Refer to the user who placed the order
-    ref: "User",                              // Refer to the User model
+    type: mongoose.Schema.Types.ObjectId,     // References the user's unique DB id
+    ref: "User",                              // Connects to the User model
     required: true,                           // UserId is required
+  },
+
+  parkName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  state: {
+    type: String,
+    required: true,
+    enum: ["WA", "OR"],
+  },
+
+  parkType: {
+    type: String,
+    required: true,
+    enum: ["NP", "NHS", "NHR", "NHP", "NRA", "NVM"],
   },
 
   parks: {
     type: [
       { type: mongoose.Schema.Types.ObjectId,     // Creates an array using the park ids
-      ref: "Park" }                               // Refers to the park model
+      ref: "Park" }                               // Connects to the park model
     ],
-    required: true,                               // ParkId is required
+    required: true,
     validate: v => v.length > 0                   // Ensures that the array is not empty
   },
 });
