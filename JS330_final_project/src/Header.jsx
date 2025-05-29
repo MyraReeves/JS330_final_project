945938dec8eb2300dfa4f84c9629c85cf83f263f
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-05-29 15:27:03 -0700

    Initial commit

---
+++ JS330_final_project/src/Header.jsx
@@ -0,0 +1,13 @@
+
+function Header() {
+
+    return (
+    <header>
+        <h1>
+            🏞 &nbsp; Visit Your National Parks! &nbsp; 🏞
+        </h1>
+    </header>
+    )
+  }
+  
+  export default Header
