a900245d00560b8acfeabd392c6d8b2f3cd76190
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-05-29 15:21:49 -0700

    Initial commit

---
+++ JS330_final_project/src/pages/Homepage.jsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+
+function Homepage() {
+    
+    return (
+        <main>
+            <div className="states">
+                <h2>Interested in visiting a location within the national park system?</h2>
+                <h3>Look up basic visitor information using our park database!</h3>
+                <br/>
+                <h4>To begin, please choose which state you will be visiting:</h4>
+                🔸 <Link to='/washington'>Washington</Link>
+                🔸 <Link to='/oregon'>Oregon</Link>
+
+                <p class="tiny-footnote">* Additional states will be added to the list as we add parks to our database</p>
+            </div>
+
+           
+        </main>
+    )
+}
+
+export default Homepage
