ad2d769a5837a74249884bb4c36542e4616b6399
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-05-31 00:31:30 -0700

    Adds map of national parks in Oregon to the page

--- front-end/src/pages/Oregon.jsx
+++ front-end/src/pages/Oregon.jsx
@@ -1,14 +1,16 @@
 import { useState } from "react"
+import { Link } from 'react-router-dom';
 import OregonDropdownMenu from "../page_components/OregonParksDropdown"
+import oregonMap from '../images/oregon-nationalparks.gif'
 
 function Oregon() {
     const [selection, setSelection] = useState('')
 
     const checkForSelection = () => {
         if (selection == ''){
-            return <p className="changeState">
-                Choose a different state
-            </p>
+            return <div className="stateMap">
+                <img src={oregonMap} alt='A map of Oregon showing the location of all national park sites' className="nothing-selected"/>
+            </div>
         }
 
         if (selection == 'crater'){
