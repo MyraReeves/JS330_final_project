8c775b7507dd0ed37c003e826375bf268b637ce4
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-05-31 03:18:10 -0700

    Adds navigation menu to header

--- front-end/src/Header.jsx
+++ front-end/src/Header.jsx
@@ -1,8 +1,17 @@
+import { Link } from 'react-router-dom'
 
 function Header() {
 
     return (
     <header>
+        <div className='dropdown-menu'>
+            <button className='dropdown-button'>🗺️  ▾</button>
+            <ul className='dropdown-content'>
+                <li><Link to='/'>Homepage </Link></li>
+                <li><Link to='/oregon'>Oregon parks </Link></li>
+                <li><Link to='/washington'>Washington parks </Link></li>
+            </ul>
+        </div>
         <h1>
             🏞 &nbsp; Visit Your National Parks! &nbsp; 🏞
         </h1>
