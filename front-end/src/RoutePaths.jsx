c471bc0d60b4352dcbfb56418e8eac93f49ad8cc
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-05-30 20:24:13 -0700

    Adds route paths for oregon & washington dropdown pages

--- front-end/src/RoutePaths.jsx
+++ front-end/src/RoutePaths.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
 
 import Homepage from './pages/Homepage';
+import Washington from './pages/Washington';
+import Oregon from './pages/Oregon';
 import PageNotFound from './pages/PageNotFound';
 
 
@@ -8,6 +10,8 @@ function RoutePaths() {
     return (
         <Routes>
           <Route path='/' element={ <Homepage/> } />
+          <Route path='/washington' element={ <Washington/> } />
+          <Route path='/oregon' element={ <Oregon/> } />
           <Route path='*' element={ <PageNotFound/> } />
         </Routes>
     )
