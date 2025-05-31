10f9b9521447e972a8fc51b7ec1586832273844c
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-05-31 02:57:59 -0700

    Adds accounts section

--- front-end/src/pages/Homepage.jsx
+++ front-end/src/pages/Homepage.jsx
@@ -8,11 +8,18 @@ function Homepage() {
                 <h2>Interested in visiting a location within the national park system?</h2>
                 <h3>Look up basic visitor information using our park database!</h3>
                 <br/>
-                <h4>To begin, please choose which state you will be visiting:</h4>
+                <h4>To begin, please choose which state you will be visiting:<br/>
                 🌲 <Link to='/oregon'>Oregon</Link><br/>
                 🌲 <Link to='/washington'>Washington</Link>
+                </h4>
 
                 <p class="tiny-footnote">* Additional states will be added to the list as we add parks to our database</p>
+            </div>
+
+            <div className = "accounts">
+                <h2>Want to keep track of which parks you've visited? <br/>
+                Or share your experiences and insights with others?</h2>
+                <h3>Sign up for an account!</h3>
             </div>
 
            
