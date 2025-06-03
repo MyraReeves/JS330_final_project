d77d9c329b5261ce89c7d13951fc6974830b8134
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-06-03 02:21:54 -0700

    Makes running the Morgan request logger conditional to only outside of Jest tests, to clean up and declutter the appearance of terminal results

--- server/app.js
+++ server/app.js
@@ -5,10 +5,14 @@ const morgan = require("morgan");       // HTTP request logger middleware for no
 
 const app = express();
 
+// Within the terminal during Jest tests, there is no need to see the block of HTTP logs that Morgan provides. So only run Morgan outside of tests:
+if (process.env.NODE_ENV !== 'test') {
+  app.use(morgan('dev'));       // Info about the request logger Morgan is at https://expressjs.com/en/resources/middleware/morgan.html
+}
+
 // Use middleware:
 app.use(cors());
-app.use(express.json());
-app.use(morgan("dev"));               // Info at https://expressjs.com/en/resources/middleware/morgan.html
+app.use(express.json()); 
 
 // Connect to routes:
 app.use("/api/auth", require("./routes/auth"));
